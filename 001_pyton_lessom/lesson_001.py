fd39f35415875421c516614bb4268fc1a156eecc
osere <o.serebrjan@gmail.com>
2022-05-15 12:16:45 +0300

    first commit

---
+++ 001_pyton_lessom/lesson_001.py
@@ -0,0 +1,44 @@
+# print hello world to console
+print(420)
+# integer (int) whole number
+print(type(420))
+# float (float)
+print (type(22.212))
+print (type(123+2.0))
+print(help (23.1))
+# integer print
+a = 250
+print(a)
+print(type(a))
+# hello world print
+a = 'hello world'
+print(a)
+print(type(a))
+# names
+name_surname = 'Oleg Serebrjan'
+# lesson -+/*
+a = 900
+print(a + a * a)
+print(type(a))
+# string (str)
+string_1 = 'oleg u4isj'
+string_2 = "hello world"
+string_3 = '''hello world'''
+string_4= """hello world"""
+print(string_1)
+print(string_1)
+# boolean (bool)
+a = True
+b = False
+print(type(a))
+print (1012 <123)
+# None type
+c = None
+print(type(c))
+x = None
+print(print(1541))
+
+
+
+
+
