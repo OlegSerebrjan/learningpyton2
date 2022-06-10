6e8740acd69b88937140cb9279b074ce332c89d5
osere <o.serebrjan@gmail.com>
2022-06-10 14:19:40 +0300

    commit

---
+++ 001_pyton_lessom/homework_005.py
@@ -0,0 +1,37 @@
+# 1
+# name = input('Please enter your name: ')
+# surname = input('Please enter your surname: ')
+# age = input('Please enter your age: ')
+# print(f'Hello, {surname} {name}. Your age is: {age}')
+# 2
+# a = float(input('Please enter side A:'))
+# b = float(input('Please enter side B:'))
+# c = (a * a + b * b) ** 0.5
+# print(c)
+# 3
+# a = float(input('Please enter side A:'))
+# b = float(input('Please enter side B:'))
+# c = float(input('Please enter side C:'))
+# d = (c ** 2)
+# e = (a * a + b * b)
+# if d == e:
+#     print('True')
+# else:
+#     print('False')
+# 4
+# lessons = ['Math', 'Programing','Biology']
+# lessons.reverse()
+# print(lessons)
+# 5
+list1 = (1, 2, 3, 5, 8)
+list2 = (8, 2, 5)
+list3 = list1.__add__(list2)
+print(list3)
+# 6
+list = [1, 2, 3, 5, 8, 8, 2, 5]
+# 7
+
+
+
+
+
